32c221144e1d49c83e467e2fa95bc19a0f7aa1b2
Kierán Meinhardt <kmein@posteo.de>
2026-01-20 19:32:50 +0100

    auto-upgrade

--- flake.nix
+++ flake.nix
@@ -35,6 +35,17 @@
                 hostname = "brainmelter.brockman.news";
               };
             }
+            {
+              system.autoUpgrade = {
+                enable = true;
+                flake = self.outPath;
+                flags = [
+                  "--print-build-logs"
+                ];
+                dates = "02:00";
+                randomizedDelaySec = "45min";
+              };
+            }
           ];
         };
       };
